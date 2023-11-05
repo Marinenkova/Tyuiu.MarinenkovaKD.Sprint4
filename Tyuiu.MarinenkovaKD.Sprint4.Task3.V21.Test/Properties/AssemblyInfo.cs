3ada8c1183e814c5cf5cc94dda4402e53766ec76
Tyuiu.MarinenkovaKD@outlook.com <marinenkova05@gmail.com>
2023-11-05 10:46:08 +0500

    Task3

---
+++ Tyuiu.MarinenkovaKD.Sprint4.Task3.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MarinenkovaKD.Sprint4.Task3.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MarinenkovaKD.Sprint4.Task3.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1723d1ce-615b-40ea-b5e0-9aad1cdc2b44")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.MarinenkovaKD.Sprint4.Task3.V21/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MarinenkovaKD.Sprint4.Task3.V21.Lib;
+
+namespace Tyuiu.MarinenkovaKD.Sprint4.Task3.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[5, 5] { { 8, 3, 3, 3, 5 },
+                                           { 6, 7, 3, 3, 7 },
+                                           { 4, 5, 4, 6, 6 },
+                                           { 5, 5, 8, 8, 7 },
+                                           { 4, 6, 6, 7, 7 } };
+
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+
+            DataService ds = new DataService();
+            
+            Console.Title = "Спринт #4 | Выполнила: Мариненкова К. Д. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы (статический ввод)                              *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнила: Мариненкова Кристина Дмитриевна | ИСТНб-23-1                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов,                    *");
+            Console.WriteLine("* заполненный статическими значениями в диапазоне от 3 до 8.              *");
+            Console.WriteLine("* Найдите произведение элементов в последнем столбце массива.             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+           
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{array[i,j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(array);
+
+            Console.WriteLine("Произведение элементов в последнем столбце = " + res);
+            Console.ReadKey();
+        }
+    }
+}
